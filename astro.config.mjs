3994b4e6c75bd6dc414f3d00b1833902e2fe2dad
Broslunas <pablo@broslunas.com>
2025-04-17 21:17:48 +0100

    feat: add React integration to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  integrations: [],
+  integrations: [react()],
   adapter: vercel(),
 });
